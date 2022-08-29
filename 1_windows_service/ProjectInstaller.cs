a0e2c4c8bb5e2241dec937a7bc97aed5860d4a61
Pasha <pashaie@gmail.com>
2022-08-29 18:18:13 +0430

    windows service

---
+++ 1_windows_service/ProjectInstaller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace _1_windows_service
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        private void serviceProcessInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+            //Service Start
+            //ServiceInstaller serviceInstaller = (ServiceInstaller) sender;
+
+
+            //using (ServiceController sc = new ServiceController("fadUpdateService"))
+            //{
+            //    sc.Start();
+            //}
+        }
+    }
+}
---
+++ 1_windows_service/Service1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace _1_windows_service
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Timer timer = new Timer(); //using System.Timers;
+            timer.Interval = 10 * 1000; // 10 * (1000 ms)
+            timer.Elapsed += Timer_Elapsed; // Add Event
+            timer.Start(); //<-- START ⭐
+
+            /*
+             * Command Prompt For VS 2022
+             *  installutil <FILE>
+             *  
+             */
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            File.WriteAllText("d:\\1.txt", $"{DateTime.Now} -- Salam");
+        }
+        
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
